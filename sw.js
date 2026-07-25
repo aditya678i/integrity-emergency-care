bbc588c8496cef871a5581402735a518218093b9
Praya <praya@example.com>
2026-07-25 21:52:29 +0530

    Force SW update to v25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'integrity-emergency-care-v24';
+const CACHE_NAME = 'integrity-emergency-care-v25';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
@@ -10,28 +10,30 @@ const ASSETS_TO_CACHE = [
   '/assets/splash-logo.png'
 ];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => {
+            return self.clients.claim();
         })
-      );
-    })
-  );
-  self.clients.claim();
+    );
 });
 
 // Network-first strategy: Always try to get the newest file from network. 
